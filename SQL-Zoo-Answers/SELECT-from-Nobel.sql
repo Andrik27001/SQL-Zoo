94d2eccb919dfe3f5bd34ba8f61e8e374337f122
Andrik Solis <Andrik27001@gmail.com>
2021-02-09 13:53:47 +0100

    Add answers 3 & 4 for test 3

--- SQL-Zoo-Answers/SELECT-from-Nobel.sql
+++ SQL-Zoo-Answers/SELECT-from-Nobel.sql
@@ -7,3 +7,14 @@ WHERE yr = 1950
 SELECT winner
 FROM nobel
 WHERE yr = 1962 AND subject = 'Literature'
+
+-- 3. Show the year and subject that won 'Albert Einstein' his prize:
+SELECT yr, subject
+FROM nobel
+WHERE winner = 'Albert Einstein'
+
+-- 4. Give the name of the 'Peace' winners since the year 2000, including 2000:
+SELECT winner
+FROM nobel
+WHERE subject = 'Peace' AND yr > 1999
+
