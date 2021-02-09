ea9cfdefea7ddbbcb4a59e53b14baad6b700c525
Andrik Solis <Andrik27001@gmail.com>
2021-02-09 13:43:10 +0100

    Add answers 3 & 4 for test 2

--- SQL-Zoo-Answers/SELECT-from-WORLD.sql
+++ SQL-Zoo-Answers/SELECT-from-WORLD.sql
@@ -6,3 +6,13 @@ FROM world
 SELECT name
 FROM world
 WHERE population > 200000000
+
+-- 3. Give the name and the per capita GDP for those countries with a population of at least 200 million:
+SELECT name, gdp/population
+FROM world
+WHERE population > 200000000
+
+-- 4. Show the name and population in millions for the countries of the continent 'South America':
+SELECT name, population/1000000
+FROM world
+WHERE continent IN ('South America')
