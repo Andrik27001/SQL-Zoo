8003f29cafe3ff10b3bdf3f0cf12b9f984b93f35
Andrik Solis <Andrik27001@gmail.com>
2021-02-09 13:52:59 +0100

    Add answers 1 & 2 for test 3

---
+++ SQL-Zoo-Answers/SELECT-from-Nobel.sql
@@ -0,0 +1,9 @@
+-- 1. Show Nobel prizes for 1950:
+SELECT yr, subject, winner
+FROM nobel
+WHERE yr = 1950
+
+-- 2. Show who won the 1962 prize for Literature:
+SELECT winner
+FROM nobel
+WHERE yr = 1962 AND subject = 'Literature'
