6ee19a4329ead60e87c9be3ccb3017a871c952c5
Andrik Solis <Andrik27001@gmail.com>
2021-02-09 13:49:34 +0100

    Add answer 13 for test 2

--- SQL-Zoo-Answers/SELECT-from-WORLD.sql
+++ SQL-Zoo-Answers/SELECT-from-WORLD.sql
@@ -57,3 +57,12 @@ SELECT name, capital
 FROM world
 WHERE LEFT(name, 1) = LEFT(capital, 1) AND name <> capital
 
+-- 13. Find the country that has all the vowels and no spaces in its name:
+SELECT name
+FROM world
+WHERE name NOT LIKE '% %'
+AND name LIKE '%a%'
+AND name LIKE '%e%'
+AND name LIKE '%i%'
+AND name LIKE '%o%'
+AND name LIKE '%u%'
