9549b58150041e154e5589d252a9b38221487605
Andrik Solis <Andrik27001@gmail.com>
2021-02-09 13:46:09 +0100

    Add answers 7 & 8 for test 2

--- SQL-Zoo-Answers/SELECT-from-WORLD.sql
+++ SQL-Zoo-Answers/SELECT-from-WORLD.sql
@@ -26,3 +26,14 @@ WHERE name IN ('France', 'Germany', 'Italy')
 SELECT name
 FROM world
 WHERE name LIKE 'United%'
+
+
+-- 7. Show the countries that are big by area or big by population. Show name, population and area:
+SELECT name, population, area
+FROM world
+WHERE area > 3000000 OR population > 250000000
+
+-- 8. Show the countries that are big by area (more than 3 million) or big by population (more than 250 million) but not both. Show name, population and area:
+SELECT name, population, area
+FROM world
+WHERE area > 3000000 XOR population > 250000000 
