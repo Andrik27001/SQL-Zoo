b994fd3af26d5d7aa0873c8f33c10903304868c9
Andrik Solis <Andrik27001@gmail.com>
2021-02-09 13:59:51 +0100

    Add answers 13 & 14 for test 3

--- SQL-Zoo-Answers/SELECT-from-Nobel.sql
+++ SQL-Zoo-Answers/SELECT-from-Nobel.sql
@@ -61,5 +61,18 @@ WHERE winner = 'PETER GRÜNBERG'
 -- 12. Find all details of the prize won by EUGENE O'NEILL:
 SELECT yr, subject, winner
 FROM nobel
-WHERE winner = 'EUGENE O\'NEILL'
+WHERE winner = "EUGENE O'NEILL" -- 'EUGENE O\'NEILL' for SQL
+
+-- 13. List the winners, year and subject where the winner starts with Sir. Show the the most recent first, then by name order:
+SELECT winner, yr, subject
+FROM nobel
+WHERE winner LIKE 'Sir%'
+ORDER BY yr DESC
+
+-- 14. Show the 1984 winners and subject ordered by subject and winner name; but list Chemistry and Physics last:
+SELECT winner, subject
+FROM nobel
+WHERE yr = 1984
+ORDER BY subject IN ('Physics', 'Chemistry'), subject, winner
+
 
