8c72e6c56d1505293efdecb075c5167377e8e642
Andrik Solis <Andrik27001@gmail.com>
2021-02-09 13:48:48 +0100

    Add answers 11 & 12 for test 2

--- SQL-Zoo-Answers/SELECT-from-WORLD.sql
+++ SQL-Zoo-Answers/SELECT-from-WORLD.sql
@@ -41,3 +41,19 @@ WHERE area > 3000000 XOR population > 250000000
 SELECT name, ROUND(population/1000000, 2), ROUND(gdp/1000000000, 2)
 FROM world
 WHERE continent IN ('South America')
+
+-- 10. Show per-capita GDP for the trillion dollar countries to the nearest $1000:
+SELECT name, ROUND(gdp/population, -3)
+FROM world
+WHERE gdp > 1000000000000
+
+-- 11. Show the name and capital where the name and the capital have the same number of characters:
+SELECT name, capital
+FROM world
+WHERE LENGTH(name) = LENGTH(capital) 
+
+-- 12. Show the name and the capital where the first letters of each match. Don't include countries where the name and the capital are the same word:
+SELECT name, capital
+FROM world
+WHERE LEFT(name, 1) = LEFT(capital, 1) AND name <> capital
+
