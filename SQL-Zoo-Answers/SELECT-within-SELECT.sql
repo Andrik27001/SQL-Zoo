66c45617fc7e37a68549c6da7cfa0ac62dfc5aee
Andrik Solis <Andrik27001@gmail.com>
2021-02-10 09:36:36 +0100

    Add answers 5 & 6 for test 4

--- SQL-Zoo-Answers/SELECT-within-SELECT.sql
+++ SQL-Zoo-Answers/SELECT-within-SELECT.sql
@@ -36,4 +36,21 @@ WHERE population > (
   WHERE name = 'Poland'
 )
 
+-- 5. Show the name and the population of each country in Europe. Show the population as a percentage of the population of Germany:
+SELECT name, CONCAT(ROUND(100 * population / (
+  SELECT population
+  FROM world
+  WHERE name = 'Germany'
+)) , '%') AS 'percentage'
+FROM world
+WHERE continent = 'Europe'
+
+-- 6. Which countries have a GDP greater than every country in Europe? [Give the name only.] (Some countries may have NULL gdp values)
+SELECT name
+FROM world
+WHERE gdp > ALL (
+  SELECT gdp 
+  FROM world
+  WHERE gdp > 0 AND continent = 'Europe'
+)
 
