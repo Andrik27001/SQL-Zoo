208c38edde26208dbef1ef02d1f73d015892b0bc
Andrik Solis <Andrik27001@gmail.com>
2021-02-10 09:39:38 +0100

    Update test 2 & 4

--- SQL-Zoo-Answers/SELECT-within-SELECT.sql
+++ SQL-Zoo-Answers/SELECT-within-SELECT.sql
@@ -1,4 +1,7 @@
+-- SELECT within SELECT Tutorial
+
 -- 1. List each country name where the population is larger than that of 'Russia':
+
 SELECT name
 FROM world
 WHERE population > (
@@ -7,7 +10,9 @@ WHERE population > (
   WHERE name='Russia'
 )
 
+
 -- 2. Show the countries in Europe with a per capita GDP greater than 'United Kingdom':
+
 SELECT name
 FROM world
 WHERE continent = 'Europe' AND gdp/population > (
@@ -16,7 +21,9 @@ WHERE continent = 'Europe' AND gdp/population > (
   WHERE name = 'United Kingdom'
 )
 
+
 -- 3. List the name and continent of countries in the continents containing either Argentina or Australia. Order by name of the country:
+
 SELECT name, continent
 FROM world
 WHERE continent IN (
@@ -25,7 +32,9 @@ WHERE continent IN (
 )
 ORDER BY name
 
+
 -- 4. Which country has a population that is more than Canada but less than Poland? Show the name and the population:
+
 SELECT name, population
 FROM world
 WHERE population > (
@@ -36,7 +45,9 @@ WHERE population > (
   WHERE name = 'Poland'
 )
 
+
 -- 5. Show the name and the population of each country in Europe. Show the population as a percentage of the population of Germany:
+
 SELECT name, CONCAT(ROUND(100 * population / (
   SELECT population
   FROM world
@@ -45,7 +56,9 @@ SELECT name, CONCAT(ROUND(100 * population / (
 FROM world
 WHERE continent = 'Europe'
 
+
 -- 6. Which countries have a GDP greater than every country in Europe? [Give the name only.] (Some countries may have NULL gdp values)
+
 SELECT name
 FROM world
 WHERE gdp > ALL (
