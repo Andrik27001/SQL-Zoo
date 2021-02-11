a1c95cab8c7d57437bc54ec397dd54f677952865
Andrik Solis <Andrik27001@gmail.com>
2021-02-11 15:49:41 +0100

    Add answers 13 & 14 for test 7

--- SQL-Zoo-Answers/More-JOIN-operations.sql
+++ SQL-Zoo-Answers/More-JOIN-operations.sql
@@ -108,4 +108,22 @@ WHERE ord = 1 AND movie.id IN (
 )
 
 
+-- 13. Obtain a list, in alphabetical order, of actors who've had at least 15 starring roles:
+
+SELECT name
+FROM casting
+JOIN actor ON (casting.actorid = actor.id)
+WHERE ord = 1
+GROUP BY name
+HAVING COUNT(ord) > 14
+
+
+-- 14. List the films released in the year 1978 ordered by the number of actors in the cast, then by title:
+
+SELECT title, COUNT(*)
+FROM movie
+JOIN casting ON (movie.id = casting.movieid)
+WHERE yr = 1978
+GROUP BY title
+ORDER BY COUNT(*) DESC, title
 
