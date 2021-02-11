814e6f029d5000c2c70dbb23fbb65dfe1849fbc7
Andrik Solis <Andrik27001@gmail.com>
2021-02-11 15:35:06 +0100

    Add answers 3 & 4 for test 6

--- SQL-Zoo-Answers/JOIN-operation.sql
+++ SQL-Zoo-Answers/JOIN-operation.sql
@@ -12,3 +12,19 @@ WHERE teamid = 'GER'
 SELECT id,stadium,team1,team2
 FROM game
 WHERE id = 1012
+
+
+-- 3. Modify it to show the player, teamid, stadium and mdate for every German goal:
+
+SELECT player, teamid, stadium, mdate
+FROM game
+JOIN goal ON (id=matchid)
+WHERE teamid = 'GER'
+
+
+-- 4. Show the team1, team2 and player for every goal scored by a player called Mario:
+
+SELECT team1, team2, player
+FROM goal
+JOIN game  ON (matchid = id)
+WHERE player LIKE 'Mario%'
