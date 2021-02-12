ee3f0de9a29131ae19075fb8ffdb37519eb2ffc0
Andrik Solis <Andrik27001@gmail.com>
2021-02-12 13:58:17 +0100

    Add answers 1 & 2 for test 8

--- SQL-Zoo-Answers/Using-Null.sql
+++ SQL-Zoo-Answers/Using-Null.sql
@@ -1,1 +1,16 @@
 -- Using Null Tutorial
+
+-- 1. List the teachers who have NULL for their department:
+
+SELECT name
+FROM teacher
+WHERE dept IS NULL
+
+
+-- 2.Note the INNER JOIN misses the teachers with no department and the departments with no teacher:
+
+SELECT teacher.name, dept.name
+FROM teacher
+JOIN dept ON (teacher.dept = dept.id)
+
+
