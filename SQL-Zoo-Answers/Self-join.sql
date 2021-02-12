8ba33304aa13bc5587c93198950964cf6173910b
Andrik Solis <Andrik27001@gmail.com>
2021-02-12 14:12:48 +0100

    Add answers 1 & 2 for test 9

--- SQL-Zoo-Answers/Self-join.sql
+++ SQL-Zoo-Answers/Self-join.sql
@@ -1,1 +1,13 @@
 -- Self join Tutorial
+
+-- 1. How many stops are in the database:
+
+Select COUNT(name)
+FROM stops
+
+
+-- 2. Find the id value for the stop 'Craiglockhart':
+
+SELECT id
+FROM stops
+WHERE name = 'Craiglockhart'
